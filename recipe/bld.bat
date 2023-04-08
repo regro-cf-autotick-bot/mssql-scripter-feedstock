7d09a1ff419b0690c24b1df6b01e2be7d6992c20
Oliver Borchert <oliver.borchert@quantco.com>
2023-04-08 03:29:41 +0200

    Try

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 mkdir mssqlscripter\mssqltoolsservice\bin\
 if errorlevel 1 exit 1
 
-tar -xf sqltoolsservice\%bin_path% -d mssqlscripter\mssqltoolsservice\bin\
+tar -xf sqltoolsservice\%bin_path% -C mssqlscripter\mssqltoolsservice\bin\
 if errorlevel 1 exit 1
 
 "%PYTHON%" -m pip install . -vv
