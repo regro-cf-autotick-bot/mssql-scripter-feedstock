aa54870a77c73cdfea6ff32e21931c100590492d
Oliver Borchert <oliver.borchert@quantco.com>
2023-04-08 02:42:03 +0200

    Update

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
-mkdir mssqlscripter/mssqltoolsservice/bin/
+mkdir mssqlscripter\mssqltoolsservice\bin\
 if errorlevel 1 exit 1
 
-unzip sqltoolsservice/%bin_path% -d mssqlscripter/mssqltoolsservice/bin/
+unzip sqltoolsservice\%bin_path% -d mssqlscripter\mssqltoolsservice\bin\
 if errorlevel 1 exit 1
 
 "%PYTHON%" -m pip install . -vv
